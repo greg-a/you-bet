02d73428a1f27ec0245196eff396ec4d954f4585
Greg Allebach <greg.allebach@gmail.com>
2021-08-29 17:54:24 -0400

    adjust script

--- frontend/server.js
+++ frontend/server.js
@@ -9,7 +9,7 @@ const handle = app.getRequestHandler();
 
 const apiPaths = {
   '/api': {
-    target: 'http://localhost:8081',
+    target: 'http://localhost:8080',
     pathRewrite: {
       '^/api': '/api'
     },
