b975be9c59dcecfae854b6d664576a8e55469557
Greg Allebach <greg.allebach@gmail.com>
2021-09-07 17:54:22 -0400

    update env name again

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -41,7 +41,7 @@ function MyApp({ Component, pageProps }) {
   }, [jwToken]);
 
   useEffect(() => {
-    console.log('environment', process.env.VERCEL_ENV)
+    console.log('environment', process.env.NEXT_PUBLIC_VERCEL_ENV)
     setJWToken(getCookie('JWToken'));
     setTimeout(() => setIsLoading(false), 500);
   }, []);
